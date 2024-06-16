b16afb733c84a13d85af0b20df76ed206b2ad0f0
Samueelx <gsammygitau@gmail.com>
2024-06-16 11:58:05 +0300

    Done with modules.

---
+++ index.js
@@ -0,0 +1,4 @@
+import getPosts, {getPostsLength} from './postController.js';
+
+console.log(getPosts())
+console.log(`Number of posts: ${getPostsLength()}`);
---
+++ postController.js
@@ -0,0 +1,13 @@
+const posts = [
+    {id: 1, title: 'Post One'},
+    {id: 2, title: 'Post Two'},
+    {id: 3, title: 'Post Three'},
+    {id: 4, title: 'Post Four'}
+];
+
+export const getPostsLength = () => posts.length;
+
+const getPosts = () => {
+    return posts;
+}
+export default getPosts;
