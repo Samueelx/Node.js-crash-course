af1e53b0aac168af0b8ca886dd941a08c5d919d5
Samueelx <gsammygitau@gmail.com>
2024-06-16 19:15:35 +0300

    Load a web pages.

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-import http from 'http'
+import http from 'http';
+import fs from 'fs/promises';
+import url from 'url'
+import path from 'path'
 
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
     try {
         if (req.method === "GET") {
+            let filepath;
             if (req.url === "/") {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write('<h1>Homepage</h1>');
-                res.end('\n');
+                filepath = path.join(__dirname, "public", "index.html"); 
             } else if (req.url === "/about") {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write('<h1>About Page</h1>');
-                res.end('\n');
+                filepath = path.join(__dirname, "public", "about.html");   
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.write('<h1>Not Found</h1>');
-                res.end('\n');
+                filepath = path.join(__dirname, "public", "404.html")
             }
+            const data = await fs.readFile(filepath)
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             throw new Error("Method not allowed")
         }
