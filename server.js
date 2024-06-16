f41faddebba34dc789e76d5d35b223a6f41a0413
Samueelx <gsammygitau@gmail.com>
2024-06-16 15:15:25 +0300

    Add basic routing.

--- server.js
+++ server.js
@@ -3,9 +3,29 @@ import http from 'http'
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify({message: "Server Error!"}));
-    res.end();
+    try {
+        if (req.method === "GET") {
+            if (req.url === "/") {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write('<h1>Homepage</h1>');
+                res.end('\n');
+            } else if (req.url === "/about") {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write('<h1>About Page</h1>');
+                res.end('\n');
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.write('<h1>Not Found</h1>');
+                res.end('\n');
+            }
+        } else {
+            throw new Error("Method not allowed")
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Server Error');
+        res.end('\n');
+    }
 });
 
-server.listen(PORT, () => {console.log(`Listening on port: ${PORT}`)});
+server.listen(PORT, () => { console.log(`Listening on port: ${PORT}`) });
