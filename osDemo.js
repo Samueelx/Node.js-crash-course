ef23f5232fbe10e7ad99d01b50101fc8f3be44ce
Samueelx <gsammygitau@gmail.com>
2024-06-24 11:11:37 +0300

    Done with os module.

---
+++ osDemo.js
@@ -0,0 +1,10 @@
+import os from 'os'
+
+/**userInfo() --> Current system user */
+console.log(os.userInfo());
+/**totalmem */
+console.log(os.totalmem());
+/**freemem */
+console.log(os.freemem());
+/**cpus() */
+console.log(os.cpus());
